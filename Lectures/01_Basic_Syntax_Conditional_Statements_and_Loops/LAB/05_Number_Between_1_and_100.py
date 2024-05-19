6314a0361bb897b4d93de7bee4caf7e304517b04
vito-dim <valyo.di@gmail.com>
2024-05-19 19:54:58 +0300

    Remove unnecessary code version

--- Lectures/01_Basic_Syntax_Conditional_Statements_and_Loops/LAB/05_Number_Between_1_and_100.py
+++ Lectures/01_Basic_Syntax_Conditional_Statements_and_Loops/LAB/05_Number_Between_1_and_100.py
@@ -12,11 +12,3 @@ while True:
 #     number = float(input())
 #
 # print(f'The number {number} is between 1 and 100')
-
-# version 3
-# number = float(input())
-#
-# while 1 >= number <= 100:
-#     number = float(input())
-#
-# print(f'The number {number} is between 1 and 100')
